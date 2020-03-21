4a64e89ad6d6edbb73f33c9fcc98af5e99882c3c
collinbartley <45106090+collinbartley@users.noreply.github.com>
2020-03-20 20:48:15 -0400

    Update deploy.sh

--- jayce/deploy.sh
+++ jayce/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<echo098>/<echo098>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:<echo098>/<jaycemakes>.git master:gh-pages
+ git push -f git@github.com:echo098/jaycemakes.git master:gh-pages
 
 cd -
